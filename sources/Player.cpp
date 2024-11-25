64e484665ed1bf81b9839b45134523e817ed77d8
Mdl44 <edy.madalin58@gmail.com>
2024-11-25 21:16:32 +0200

    Test

--- sources/Player.cpp
+++ sources/Player.cpp
@@ -4,7 +4,7 @@ Player::Player(Team* my_team) : my_team(my_team) {
     if(my_team) my_team->set_control(true);
 }
 
-bool Player::swap_try(Driver* const& my_driver, Driver* const& other_driver, Team& other_team) const {
+bool Player::swap_try(const Driver* const& my_driver, const Driver* const& other_driver, Team& other_team) const {
     if (my_team) {
         return my_team->swap(my_driver, other_driver, other_team);
     }
