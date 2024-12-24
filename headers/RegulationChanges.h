eedc3bc4b6e12e7d4f2bbf88c2fcd4804c0d807b
Mdl44 <edy.madalin58@gmail.com>
2024-12-24 13:56:27 +0200

    bife

--- headers/RegulationChanges.h
+++ headers/RegulationChanges.h
@@ -7,7 +7,6 @@ class Team;
 
 class RegulationChanges {
     static int calculate_reduction(int stats);
-    const int max_stats = 225;
 public:
     static void apply_regulation_changes(const std::vector<std::unique_ptr<Team>>& teams);
 
